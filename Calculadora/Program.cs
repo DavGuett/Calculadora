29d802e9916c3df76e484cb7c873b30a41dcd927
DavGuett <soulsec@protonmail.com>
2022-10-02 21:08:30 -0300

    Divisão finalizada

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -24,6 +24,16 @@ while (executar)
             Console.WriteLine("A soma dos dois números é: " + resultado);
             break;
 
+        case 2:
+            Console.WriteLine("Digite os números que deseja dividir (máximo 2)");
+            Console.Write("Número 1: ");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Número 2: ");
+            numero2 = int.Parse(Console.ReadLine());
+            resultado = Dividir(numero1, numero2);
+            Console.WriteLine("A divisão dos dois números é: " + resultado);
+            break;
+
         case 3:
             Console.WriteLine("Digite os números que deseja subtrair (máximo 2)");
             Console.Write("Número 1: ");
@@ -49,6 +59,12 @@ while (executar)
 static int Adicionar(int numero1, int numero2)
 {
     int resultado = numero1 + numero2;
+    return resultado;
+}
+
+static int Dividir (int numero1, int numero2)
+{
+    int resultado = numero1 / numero2;
     return resultado;
 }
 
